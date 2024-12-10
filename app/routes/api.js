56806636439844c2a670163e9e90f1dd25a70374
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-10 07:56:50 -0800

    Update api.js

--- app/routes/api.js
+++ app/routes/api.js
@@ -11,17 +11,6 @@ router.get('/api', function(req, res) {
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post('/api', function(req, res) {
-  feedbackData.unshift(req.body);
-  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
-    if (err) {
-      console.log(err);
-    }
-  });
-  res.json(feedbackData);
-});
-
-
 router.delete('/api/:id', function(req, res) {
   feedbackData.splice(req.params.id, 1);
   fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
@@ -32,6 +21,14 @@ router.delete('/api/:id', function(req, res) {
   res.json(feedbackData);
 });
 
-
+router.post('/api', function(req, res) {
+  feedbackData.unshift(req.body);
+  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.json(feedbackData);
+});
 
 module.exports = router;
