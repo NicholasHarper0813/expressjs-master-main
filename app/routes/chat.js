90bfded147818fc40a0040221c089675b1bbb264
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-10 07:57:16 -0800

    Update chat.js

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,13 +1,11 @@
+var router = express.Router(); //chat+modify
 var express = require('express');
-var router = express.Router();
 
 router.get('/chat', function(req, res) {
-
   res.render('chat', {
     pageTitle: 'Chat',
     pageID: 'chat'
   });
-
 });
 
 module.exports = router;
